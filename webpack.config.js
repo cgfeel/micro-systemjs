e78285120ac0d4d6cab0da77f7ac241fa23e7f3b
levi <cgfeel@gmail.com>
2026-07-11 17:07:57 +0800

    fix: update webpack Compiler

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,129 @@
-const { HtmlWebpackPlugin, loader } = require("@event-chat/micro-dev-config/helpers");
-const path = require("path");
+const { HtmlWebpackPlugin, copyPlugin, defineEnvPlugin, loader } = require("@event-chat/micro-dev-config/helpers");
+const path = require('path')
 
-module.exports = env => {
-    return {
-        // 1.为了更好的看到打包后的代码，统一设置 mode 为开发模式
-        mode: "development",
-        // entry: "./src/index.tsx",
-        output: {
-            filename: "index.js",
-            path: path.resolve(__dirname, "dist"),
+const distPath = path.resolve(__dirname, 'dist')
+const extensions = [".tsx", ".ts", ".jsx", ".js"]
 
-            // 2.指定生产模式下采用 systemjs 模块规范
-            libraryTarget: env.production ? "system" : ""
+// 共用 babel/ts 规则
+const rules = [
+    {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: { 
+            loader: "babel-loader",
+            options: {
+                presets: [
+                    "@babel/preset-env",
+                    "@babel/preset-react"
+                ],
+                plugins: [
+                    ["@babel/plugin-transform-runtime"]
+                ],
+            }
+        }
+    },
+    // 3.5 对于 ts 先用 ts-load 转换之后再交给 babel
+    {
+        test: /\.tsx?$/,
+        use: {
+            loader: loader("ts-loader")
         },
-        // 2.5 可以不写入口 entry，默认 src/index，但是如果要支持 tsx 就要添加 extension
-        resolve: {
-            extensions: [".tsx", ".ts", ".jsx", ".js"],
-        },
-        module: {
-            rules: [
-                // 3.使用 babel 解析 js 文件
-                {
-                    test: /\.m?js$/,
-                    exclude: /node_modules/,
-                    use: { 
-                        loader: "babel-loader",
-                        options: {
-                            presets: [
-                                "@babel/preset-env",
-                                "@babel/preset-react"
-                            ],
-                            plugins: [
-                                ["@babel/plugin-transform-runtime"]
-                            ],
-                        }
-                    }
-                }, 
-                // 3.5 对于 ts 先用 ts-load 转换之后再交给 babel
-                {
-                    test: /\.tsx?$/,
-                    use: {
-                        loader: loader("ts-loader")
-                    },
-                    exclude: /node_modules/
-                }
-            ]
-        },
-        plugins: [
-            // 4.生产环境不生成 html
-            !env.production && new HtmlWebpackPlugin({
-                template: "./public/index.html"
-            })
-        ].filter(Boolean),
-
-        // 5.生产环境下不打包 react, react-dom
-        externals: env.production ? ["react", "react-dom"] : []
+        exclude: /node_modules/
     }
-};
+]
+
+const baseConfig = {
+    mode: 'development',
+    resolve: { extensions },
+    module: { rules }
+}
+
+const reactSystemConfig = {
+    mode: 'development',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        library: { type: 'system' }
+    }
+}
+
+const buildLoader = (APP_NAME, BASE_URL = '/') => ({
+    ...baseConfig,
+    name: 'loader',
+    entry: APP_NAME === 'system-custom' ? {
+        base: './src/BaseApp.tsx',
+        'custom-build': './src/loader.ts'
+    } : {
+        base: './src/BaseApp.tsx',
+        build: './src/loader.ts'
+    },
+    output: {
+        filename: '[name].js',
+        path: distPath
+    },
+    plugins: [
+        defineEnvPlugin({}, { APP_NAME }),
+        APP_NAME === 'system-custom' && copyPlugin([
+            {
+                from: path.resolve(__dirname, 'public'),
+                noErrorOnMissing: true,
+                globOptions: {
+                    ignore: ['**/index.html']
+                },
+            },
+            {
+                from: require.resolve('systemjs/dist/system.min.js'),
+                to: 'system.js'
+            }
+        ]),
+        new HtmlWebpackPlugin({
+            chunks: APP_NAME === 'system-custom' ? ['base', 'custom-build'] : ['base', 'build'],
+            filename: APP_NAME === 'system-custom' ? 'custom.html' : 'index.html',
+            template: "./public/index.html",
+            templateParameters: { BASE_URL }
+        })
+    ].filter(Boolean)
+})
+
+module.exports = env => [
+    // ======== 构建 React ========
+    {
+        ...reactSystemConfig,
+        entry: {
+            react: 'react'
+        }
+    },
+    {
+        ...reactSystemConfig,
+        entry: {
+            'react-dom': 'react-dom'
+        },
+        externals: ['react']
+    },
+    {
+        ...baseConfig,
+        entry: "./src/facade.ts",
+        output: {
+            filename: 'custom-system.js',
+            library: {
+                type: 'window',
+                name: 'System'
+            }
+        }
+    },
+    // ======== 构建 A：业务代码 → dist/index.js ========
+    {
+        ...baseConfig,
+        name: 'app',
+        entry: './src/index.tsx',
+        output: {
+            filename: 'index.js',
+            path: distPath,
+            libraryTarget: 'system'
+        },
+        externals: ['react', 'react-dom']
+    },
+    // ======== 构建 B：loader + HTML + 复制静态依赖 ========
+    buildLoader('system-origin', env.production ? "/micro-single-spa-app/" : "/"),
+    buildLoader('system-custom', env.production ? "/micro-single-spa-app/" : "/")
+]
